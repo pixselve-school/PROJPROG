b7077147391f344bfe5366ef447c781cfa125f77
yanis <yanis.bouger@gmail.com>
2022-05-19 16:16:19 +0200

    Changes by rgran

---
+++ src/tile/Wall_front.java
@@ -0,0 +1,27 @@
+package tile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Wall_front extends Wall {
+    private static final BufferedImage image;
+
+    static {
+        try {
+            image = ImageIO.read(Objects.requireNonNull(Wall.class.getResource("/tiles/WALL_front.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Wall_front(int x, int y){
+        super( x, y);
+    }
+
+    @Override
+    protected BufferedImage getImage() {
+        return image;
+    }
+}
