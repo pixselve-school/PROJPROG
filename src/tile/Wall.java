280a31a611604e8fae77b27ad73e1302548d6e21
yanis <yanis.bouger@gmail.com>
2022-05-19 17:17:15 +0200

    portal et textures

--- src/tile/Wall.java
+++ src/tile/Wall.java
@@ -8,23 +8,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class Wall extends Tile {
-    private static final BufferedImage image;
-
-    static {
-        try {
-            image = ImageIO.read(Objects.requireNonNull(Wall.class.getResource("/tiles/BRICK2.png")));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+public abstract class Wall extends Tile {
     public Wall(int x, int y){
         super(true, x, y);
-    }
-
-    @Override
-    protected BufferedImage getImage() {
-        return image;
     }
 }
